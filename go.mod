3222f2e09cc52cade2f81fe2a55593331b6ba73b
Amirabbas Fazelinia <utilyre@gmail.com>
2024-01-09 13:11:49 +0330

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/go-htmx-tailwindcss
+
+go 1.21.5
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./{pages,components}/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
