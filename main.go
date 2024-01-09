a20038d7691f7b6d8b7bd237b6a39964f352ef2a
Amirabbas Fazelinia <utilyre@gmail.com>
2024-01-09 22:29:19 +0330

    chore(todo): remove finished todos

--- main.go
+++ main.go
@@ -3,8 +3,5 @@ package main
 import "example.com/go-htmx-tailwindcss/application"
 
 func main() {
-	// TODO: Root = Root or CWD
-	// TODO: initialize echo with binder and validator
-
 	_ = application.New()
 }
