c1bff488f25de1e05a2e1885d833e034775ccc20
Amirabbas Fazelinia <utilyre@gmail.com>
2024-01-10 21:51:45 +0330

    better ellipsis

--- model/attraction.go
+++ model/attraction.go
@@ -5,8 +5,9 @@ import "github.com/uptrace/bun"
 type Attraction struct {
 	bun.BaseModel `bun:"table:attractions"`
 
-	ID      int64  `bun:"id,pk,autoincrement"`
-	Title   string `bun:"title"`
-	Brief   string `bun:"brief"`
-	Content string `bun:"content"`
+	ID       int64  `bun:"id,pk,autoincrement"`
+	ImageURL string `bun:"image_url"`
+	Title    string `bun:"title"`
+	Brief    string `bun:"brief"`
+	Content  string `bun:"content"`
 }
